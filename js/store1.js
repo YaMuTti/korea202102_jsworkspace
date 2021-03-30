9aaa209d7727009458470fc1c4daf291745567b4
YaMuTti <duddnjsyh@gmail.com>
2021-03-30 17:49:02 +0900

    no message

---
+++ js/store1.js
@@ -0,0 +1,52 @@
+/*역삼동 맛집 정보 json*/
+var store1={
+    "local":"역삼동",
+    "theme":"맛집",
+    "storeList":[
+       {
+           "name":"밀빵떡볶이",
+           "latLng":{
+                "lat":37.50091705137511, 
+                "lng":127.03310937358229
+           },
+           "image":"../images/foodplace/200.PNG",
+           "review":"20년간 장사를 해온 이집은 최고의 맛집"
+       },        
+       {
+           "name":"더후라이팬",
+           "latLng":{
+                "lat":37.501294236579184, 
+                "lng":127.0330118102085
+           },
+           "image":"../images/foodplace/dubu.PNG",
+           "review":"후라이팬 맛좀 볼텨"
+       },        
+       {
+           "name":"삼곱식당",
+           "latLng":{
+                "lat":37.50269170628681, 
+                "lng":127.03721643328917
+           },
+           "image":"../images/foodplace/gansai.PNG",
+           "review":"삼곱하기삼은"
+       },        
+       {
+           "name":"이태리부대찌게",
+           "latLng":{
+                "lat":37.502935868017275, 
+                "lng":127.03815809523849
+           },
+           "image":"../images/foodplace/house.PNG",
+           "review":"빈센조까사노찌개"
+       },        
+       {
+           "name":"우리집",
+           "latLng":{
+                "lat":37.503329079071506, 
+                "lng":127.03884020356904
+           },
+           "image":"../images/foodplace/jin.PNG",
+           "review":"라면이최곤줄알았지"
+       }
+    ]
+}
